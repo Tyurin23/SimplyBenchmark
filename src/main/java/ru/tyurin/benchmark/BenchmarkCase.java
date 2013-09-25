0b5eda00337dde8c107a35901346671b35d100ab
tyurin <tyurin23@gmail.com>
2013-09-25 17:03:35 +0400

    refactoring

--- src/main/java/ru/tyurin/benchmark/BenchmarkCase.java
+++ src/main/java/ru/tyurin/benchmark/BenchmarkCase.java
@@ -5,52 +5,73 @@ import java.util.*;
 
 public class BenchmarkCase {
 
-	private List<Benchmark> benchmarks;
-	private Map<Benchmark, Collection<BenchmarkResult>> results;
+	private Set<Benchmark> benchmarks;
+	private Map<Benchmark, List<BenchmarkResult>> results;
 	private Map<Benchmark, Integer> counts;
 	private String name = "";
 
+	/**
+	 * Create Benchmark Case
+	 */
 	public BenchmarkCase() {
-		benchmarks = new ArrayList<Benchmark>();
-		results = new HashMap<Benchmark, Collection<BenchmarkResult>>();
+		benchmarks = new HashSet<Benchmark>();
+		results = new HashMap<Benchmark, List<BenchmarkResult>>();
 		counts = new HashMap<Benchmark, Integer>();
 	}
 
+	/**
+	 * Create named benchmark case
+	 *
+	 * @param name benchmark case name
+	 * @see #setName
+	 */
 	public BenchmarkCase(String name) {
 		this();
 		setName(name);
 	}
 
+	/**
+	 * Get benchmark case name
+	 *
+	 * @return case name
+	 */
 	public String getName() {
 		return name;
 	}
 
-	public Collection<BenchmarkResult> getResults() {
-		Collection<BenchmarkResult> results = new ArrayList<BenchmarkResult>();
-		for (Collection<BenchmarkResult> resultList : this.results.values()) {
-			results.addAll(resultList);
-		}
-		return results;
-	}
-
-	public Collection<BenchmarkResult> getResults(Benchmark benchmark) {
-		return this.results.get(benchmark);
-	}
-
-	public Collection<Benchmark> getBenchmarks() {
-		return benchmarks;
-	}
-
+	/**
+	 * Set benchmark case name
+	 * <p/>
+	 * If name is null, then name - empty string
+	 *
+	 * @param name case name
+	 */
 	public void setName(String name) {
 		if (name != null) {
 			this.name = name;
 		}
 	}
 
+	/**
+	 * Add benchmark in test case
+	 *
+	 * @param benchmark
+	 * @throws NullPointerException If  benchmark is null
+	 */
 	public void addBenchmark(Benchmark benchmark) {
 		addBenchmark(benchmark, 1);
 	}
 
+	/**
+	 * Add benchmark in test case.
+	 * <p/>
+	 * If benchmark already exist, it wont be added, but count will be changed
+	 *
+	 * @param benchmark
+	 * @param count     Count of starts test
+	 * @throws NullPointerException     If  benchmark is null
+	 * @throws IllegalArgumentException If count is negative
+	 */
 	public void addBenchmark(Benchmark benchmark, int count) {
 		if (benchmark == null) {
 			throw new NullPointerException("Benchmark is null");
@@ -58,15 +79,52 @@ public class BenchmarkCase {
 		if (count < 0) {
 			throw new IllegalArgumentException("Count value cannot be negative");
 		}
-		benchmarks.add(benchmark);
-		counts.put(benchmark, count);
+		addBenchmarkTest(benchmark);
+		setCount(benchmark, count);
 	}
 
+	/**
+	 * Set count of benchmark runs
+	 *
+	 * @param benchmark
+	 * @param count     Count of starts test
+	 * @throws NullPointerException     If  benchmark is null
+	 * @throws IllegalArgumentException If count is negative or if benchmark not found
+	 */
+	public void setBenchmarkCount(Benchmark benchmark, int count) {
+		if (benchmark == null) {
+			throw new NullPointerException("Benchmark is null");
+		}
+		if (count < 0) {
+			throw new IllegalArgumentException("Count < 0");
+		}
+		if (!hasBenchmark(benchmark)) {
+			throw new IllegalArgumentException("Benchmark not found");
+		}
+		setCount(benchmark, count);
+	}
+
+	/**
+	 * Remove benchmark from case
+	 *
+	 * @param benchmark
+	 * @return True if test removed, or False if not.
+	 */
+	public boolean removeBenchmark(Benchmark benchmark) {
+		counts.remove(benchmark);
+		return benchmarks.remove(benchmark);
+	}
+
+	/**
+	 * Run test case
+	 * <p/>
+	 * Old result will be cleared
+	 */
 	public void run() {
 		results.clear();
 		for (Benchmark benchmark : benchmarks) {
-			int count = counts.get(benchmark);
-			Collection<BenchmarkResult> res = new ArrayList<BenchmarkResult>(count);
+			int count = getCount(benchmark);
+			List<BenchmarkResult> res = new ArrayList<BenchmarkResult>();
 			for (int c = 0; c < count; c++) {
 				BenchmarkResult result = benchmark.run();
 				res.add(result);
@@ -79,6 +137,45 @@ public class BenchmarkCase {
 		}
 	}
 
+	/**
+	 * Get result of last test
+	 *
+	 * @return Collection of results
+	 */
+	public Collection<BenchmarkResult> getResults() {
+		Collection<BenchmarkResult> results = new ArrayList<BenchmarkResult>();
+		for (Collection<BenchmarkResult> resultList : this.results.values()) {
+			results.addAll(resultList);
+		}
+		return results;
+	}
+
+	/**
+	 * Get result of last benchmark test
+	 *
+	 * @param benchmark
+	 * @return Collection of results
+	 */
+	public Collection<BenchmarkResult> getResults(Benchmark benchmark) {
+		return this.results.get(benchmark);
+	}
+
+	/**
+	 * Get benchmarks
+	 *
+	 * @return Collection of benchmarks
+	 */
+	public Collection<Benchmark> getBenchmarks() {
+		return benchmarks;
+	}
+
+	/**
+	 * Get summary string
+	 * <p/>
+	 * Return string with all results of last test
+	 *
+	 * @return
+	 */
 	public String getSummary() {
 		StringBuilder builder = new StringBuilder(String.format("Benchmark Case %s%n", getName()));
 		builder.append("\n");
@@ -92,6 +189,24 @@ public class BenchmarkCase {
 			}
 		}
 		return builder.toString();
+	}
+
+
+	protected int getCount(Benchmark benchmark) {
+		return counts.get(benchmark);
+	}
+
+	protected void setCount(Benchmark benchmark, int count) {
+		counts.put(benchmark, count);
+	}
+
+	protected void addBenchmarkTest(Benchmark benchmark) {
+		if (!hasBenchmark(benchmark))
+			benchmarks.add(benchmark);
+	}
+
+	protected boolean hasBenchmark(Benchmark benchmark) {
+		return benchmarks.contains(benchmark);
 	}
 
 	private BenchmarkResult getTotal(Collection<BenchmarkResult> results, Benchmark benchmark) {
--- src/test/java/ru/tyurin/benchmark/BenchmarkCaseTest.java
+++ src/test/java/ru/tyurin/benchmark/BenchmarkCaseTest.java
@@ -4,84 +4,69 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Random;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
-/**
- * User: tyurin
- * Date: 9/20/13
- * Time: 1:13 PM
- */
 public class BenchmarkCaseTest {
 
-	String f1 = "f1";
-	String f2 = "f2";
-	String f3 = "f3";
-	byte[] data;
-	Runnable r1, r2, r3;
+	final String name = "TEST";
+
+	final Runnable EMPTY = new Runnable() {
+		@Override
+		public void run() {
+		}
+	};
 
 	@BeforeMethod
 	public void setUp() throws Exception {
-		Random random = new Random();
-		data = new byte[8096];
-		random.nextBytes(data);
 
-		r1 = new Runnable() {
-			@Override
-			public void run() {
-				File f = new File(f1);
-				try {
-					f.createNewFile();
-					FileOutputStream outputStream = new FileOutputStream(f);
-					outputStream.write(data);
-					outputStream.flush();
-					outputStream.close();
-					f.delete();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		};
 
-		r2 = new Runnable() {
-			@Override
-			public void run() {
-				Path p = Paths.get(f2);
-				try {
-					Files.createFile(p);
-					OutputStream out = Files.newOutputStream(p);
-					out.write(data);
-					out.flush();
-					out.close();
-					Files.delete(p);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		};
 	}
 
 	@AfterMethod
 	public void tearDown() throws Exception {
+
+
 	}
 
 	@Test
-	public void testBenchmarkCase() throws Exception {
-		Benchmark b1 = new Benchmark(r1, "b1");
-		Benchmark b2 = new Benchmark(r2, "b2");
-		BenchmarkCase benchCase = new BenchmarkCase();
-		benchCase.addBenchmark(b1);
-		benchCase.addBenchmark(b2, 2);
+	public void testConstructor() throws Exception {
+		new BenchmarkCase();
+		new BenchmarkCase("TEST");
+	}
 
-		benchCase.run();
+	@Test
+	public void testConstructorName() throws Exception {
+		BenchmarkCase benchmarkCase = new BenchmarkCase(name);
 
-		System.out.println(benchCase.getSummary());
-		System.out.println(benchCase.getResults().size());
+		assertEquals(name, benchmarkCase.getName());
+	}
+
+	@Test
+	public void testConstructorNameNull() throws Exception {
+		BenchmarkCase benchmarkCase = new BenchmarkCase(null);
+
+		assertEquals("", benchmarkCase.getName());
+	}
+
+	@Test
+	public void testSetName() throws Exception {
+		BenchmarkCase benchmarkCase = new BenchmarkCase();
+
+		benchmarkCase.setName(name);
+
+		assertEquals(name, benchmarkCase.getName());
+	}
+
+	@Test
+	public void testAddBenchmark() throws Exception {
+		BenchmarkCase benchmarkCase = new BenchmarkCase();
+		Benchmark benchmark = new Benchmark(EMPTY);
+
+		benchmarkCase.addBenchmark(benchmark);
+
+		assertEquals(1, benchmarkCase.getBenchmarks().size());
+		assertTrue(benchmarkCase.getBenchmarks().contains(benchmark));
+
 	}
 }
